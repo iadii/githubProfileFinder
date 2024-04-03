6da7d25e399e1e18877b798a92d6e3c5cf5e55b3
Aditya Mishra <mishraaditya3456@gmail.com>
2024-04-03 23:06:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'Rose-Quartz':'#BEA8AA'
+      },
+    },
+  },
+  plugins: [],
+}
+
